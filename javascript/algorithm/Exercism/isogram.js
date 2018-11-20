12dc82db211eb0500c13c76366da8af29c74dde9
Kimjiwon <jhd1925@gmail.com>
2018-11-20 19:14:47 +0900

    add a Exercism algoritm

---
+++ javascript/algorithm/Exercism/isogram.js
@@ -0,0 +1,10 @@
+
+function isIsogram(input) {
+    if (input !== '') return true
+    const a = input.toLowerCase().match(/[a-z]/ig).sort()
+    const b = a.filter((item, index) => item !== a[index - 1])
+    return a.length === b.length ? true : false
+
+}
+
+export { isIsogram }
