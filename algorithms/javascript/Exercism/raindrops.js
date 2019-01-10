d1a87e0fc0a2c7121681a3e6b808152d81dcf1fa
Kimjiwon <jhd1925@gmail.com>
2019-01-10 22:56:54 +0900

    190110 TIL: add an Exercism algorithm

---
+++ algorithms/javascript/Exercism/raindrops.js
@@ -0,0 +1,9 @@
+export default class Raindrops {
+  convert(input) {
+    let result = "";
+    if (input % 3 === 0) result += "Pling";
+    if (input % 5 === 0) result += "Plang";
+    if (input % 7 === 0) result += "Plong";
+    return result || input.toString();
+  }
+}
