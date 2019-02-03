59b342f1e7f14b25876c074f32d01cf765331560
Kimjiwon <jhd1925@gmail.com>
2019-02-03 23:52:34 +0900

    190203 TIL: edit an Programmers algorithm

--- algorithms/javascript/programersCodingTest/level02/joystick.js
+++ algorithms/javascript/programersCodingTest/level02/joystick.js
@@ -32,16 +32,21 @@ function isWillChange(index, order, initial, name) {
   return isWillChange(index, order + 1, initial, name);
 }
 
-// 다른사람 풀이 (속도 3 - 4)
+// 다른사람 풀이 (속도 3 - 4, "BBAAAAAAAAABB" 에서 오류)
 function solution(name) {
+  // a를 중점으로 둔 알파벳 문자열을 선언한다.
   var map = "nopqrstuvwxyzabcdefghijklm".toUpperCase();
+  // name값을 배열로 만든다.
   var NS = name.split("");
+  // 결과가 될 answer 값
   var answer = 0;
 
+  //
   var lastA = 0;
   var firstA = 0;
   //좌우 탐색
   for (var i = 1; i < NS.length; i++) {
+    // name의 1번 인덱스부터 "A"의 갯수를 찾는다.
     if (NS[i] !== "A") {
       break;
     } else {
@@ -49,6 +54,7 @@ function solution(name) {
     }
   }
   for (var i = NS.length - 1; i > -1; i--) {
+    // name의 마지막 인덱스부터 "A"의 갯수를 찾는다.
     if (NS[i] !== "A") {
       break;
     } else {
@@ -56,18 +62,24 @@ function solution(name) {
     }
   }
 
+  // 만약 A의 갯수가 처음(index 1)과 끝에서 0개 이상이 연속된다면
   if (lastA > 0 || firstA > 0) {
+    // 끝에 연속된 A가 많다면
     if (lastA > firstA) {
+      // name을 마지막에 A가 없도록 자른다.
       NS = NS.slice(0, NS.length - lastA);
+      // index 1번부터 연속된 A가 많다면
     } else {
+      // index 1번 부터 연속된 A를 자른다.
       NS.splice(1, firstA);
     }
   }
 
+  // 남은 name 배열에서 상하 조이스틱의 횟수를 반환될 정답에 더한다.
   NS.map(a => {
-    console.log(map.indexOf(a));
     answer += Math.abs(map.indexOf(a) - 13);
   });
 
+  // 상하로 움직인 횟수와 남은 name 배열의 갯수 - 1개를 한 값이 정답이다.
   return answer + NS.length - 1;
 }
