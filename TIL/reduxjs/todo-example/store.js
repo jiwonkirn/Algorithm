4208e83c693d2555fdb8113cf65ef13aac1bbf68
Kimjiwon <jhd1925@gmail.com>
2019-02-12 14:02:14 +0900

    190212 TIL: update redux todo example

--- TIL/reduxjs/todo-example/store.js
+++ TIL/reduxjs/todo-example/store.js
@@ -1,53 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import todoApp from "./ducks";
-import thunk from "redux-thunk";
-
-const addLoggingToDispatch = store => {
-  const rawDispatch = store.dispatch;
-
-  if (!console.group) {
-    return rawDispatch;
-  }
-
-  return action => {
-    console.group(action.type);
-    console.log("%c prev state", "color: gray", store.getState());
-    console.log("%c action", "color: blue", action);
-    const returnValue = rawDispatch(action);
-    console.log("%c next state", "color: green", store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
-
-const addPromiseSupportToDispatch = store => {
-  const rawDispatch = store.dispatch;
-
-  return action => {
-    if (typeof action.then === "function") {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch;
-  };
-};
+import createLogger from "redux-logger";
+import promise from "redux-promise";
 
 const configureStore = () => {
-  // const middleware = [thunk];
-
-  const store = createStore(todoApp);
-
-  // "production"은 배포 모드
-  // "development"는 개발 모드이다.
+  const middlewares = [promise];
   if (process.env.NODE_ENV !== "production") {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(createLogger);
   }
 
-  // 순서가 addLoadingToDispatch 위로 가게되면, return된 promise 객체가 action으로 들어간다.
-  store.dispatch = addPromiseSupportToDispatch(store);
-
-  console.log(store.dispatch);
-
-  return store;
+  return createStore(todoApp, applyMiddleware(...middlewares));
 };
 
 export default configureStore;
