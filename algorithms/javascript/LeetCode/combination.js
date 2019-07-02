56a0e09ed09350ab5a3b3ff6bed666d629793dc4
Kimjiwon <jhd1925@gmail.com>
2019-07-02 16:09:26 +0900

    edit leetcoe algorithm - Combination

--- algorithms/javascript/LeetCode/combination.js
+++ algorithms/javascript/LeetCode/combination.js
@@ -64,3 +64,33 @@ var combine = function(n, k) {
 
   return ans;
 };
+
+// 다른 사람의 풀이를 보고
+// 알고리즘을 이해한 뒤 내가 푼 알고리즘
+var combine = function(n, k) {
+  let result = [],
+    cur = [];
+  for (var i = 1; i <= k; i++) {
+    cur.push(i);
+  }
+  result.push(cur);
+  while (cur) {
+    const nextCur = [...cur, n + 1];
+    let j;
+    for (j = n - 2; j >= 0; j--) {
+      if (nextCur[j] + 1 < nextCur[j + 1]) break;
+    }
+    if (j < 0) {
+      cur = null;
+      break;
+    }
+    let num = nextCur[j];
+    for (; j < nextCur.length - 1; j++) {
+      nextCur[j] = ++num;
+    }
+    nextCur.pop();
+    cur = nextCur;
+    result.push(cur);
+  }
+  return result;
+};
