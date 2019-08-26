6a957327262fee7dac71dff14f6a3a7dfc5d7d5a
Kimjiwon <jhd1925@gmail.com>
2019-08-26 15:19:00 +0900

    [190826 Leetcode] add a solution: Two sum

--- LeetCode/Two_Sum.js
+++ LeetCode/Two_Sum.js
@@ -1,5 +1,21 @@
+// first
+// O(n ** 2)
 const twoSum = function(nums, target) {
   for (let i = 0; i < nums.length; i++)
     for (let j = i + 1; j < nums.length; j++)
       if (nums[i] + nums[j] === target) return [i, j];
+};
+
+// second
+// O(n)
+const twoSum = function(nums, target) {
+  const obj = {};
+  for (let i = 0; i < nums.length; i++) {
+      const elem = target - nums[i];
+      if (obj[elem] == null) {
+          obj[nums[i]] = i;
+      } else {
+          return [obj[elem], i];
+      }
+  }
 };
