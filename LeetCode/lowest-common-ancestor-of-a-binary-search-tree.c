d95d1f00a6cd7871ed3bcb5632904a62b15be18d
jiwonkirn <jhd1925@gmail.com>
2019-12-14 22:59:10 +0900

    [191214 Leetcode] Lowest common ancestor of a binary search tree (EASY, C)

---
+++ LeetCode/lowest-common-ancestor-of-a-binary-search-tree.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct TreeNode
+{
+  int val;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
+
+struct TreeNode *lowestCommonAncestor(struct TreeNode *root, struct TreeNode *p, struct TreeNode *q)
+{
+  if (root->val > p->val && root->val > q->val)
+    return lowestCommonAncestor(root->left, p, q);
+  if (root->val < p->val && root->val < q->val)
+    return lowestCommonAncestor(root->right, p, q);
+  return root;
+}
